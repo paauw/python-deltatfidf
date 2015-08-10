9186b1c402f3c1106defa6f4aab09dcddd364158
Tim Paauw <timpaauw@gmail.com>
2015-08-10 14:12:27 +0200

    Clean code

--- vectorizer.py
+++ vectorizer.py
@@ -3,15 +3,12 @@ import numpy as np
 import math
 
 # from sklearn.ensemble import RandomForestClassifier
-# from sklearn.svm import SVC
-# c1 = [ 'dit is de eerste text', 'de is de tweede text nog wat info', 'dit is de derde text wat grappig dit henk'];
-# c2 = [ 'dit is de tweede class text', 'jaja wat een uitstekende test', 'jaja dit is lastig zeg'];
 
-# print start[1].split().count('de')
+# c1 = [ 'this is the first text', 'this is the second text and some info', 'this is the third text'];
+# c2 = [ 'this is text for the second class', 'just to test', 'repeating test test test'];
 
 class deltaTFIDF:
-	def __init__(self, class1, class2, max_features=0, treshold=0.1):
-		self.treshold = treshold
+	def __init__(self, class1, class2, max_features=0):
 		self.limit = max_features
 		self.classP = class1
 		self.classN = class2
@@ -35,13 +32,6 @@ class deltaTFIDF:
 		if self.limit:
 			self.features = sorted(self.features, key=lambda x: abs(self.cc_dict[x]['log']), reverse=True)[:self.limit]
 
-			# We throw out anything below the treshold, because the really low
-			# log values mean that there is almost no difference between classes
-		# 	if self.cc_dict[term]['log'] < self.treshold:
-		# 		self.below_thres.append(term)
-
-		# self.features = [a for a in self.features if a not in self.below_thres]
-
 	def vector(self, text):
 		text = text.split()
 		vector = []
@@ -60,13 +50,10 @@ class deltaTFIDF:
 			matrix.append( self.vector( text ))
 		return matrix
 
-#delta = deltaTFIDF(c1,c2, max_features=5)
+# delta = deltaTFIDF(c1,c2, max_features=5)
 # training = delta.vectorize(c1 + c2)
 
-# model = RandomForestClassifier(n_estimators = 100)
-# #model = SVC(probability=1)
+# model = RandomForestClassifier()
 # model.fit( training, [1,1,1,0,0,0])
 
-# print sorted(delta.features, key=lambda x:delta.cc_dict[x])
-# print delta.vector('henk henk')
-# print model.predict_proba( delta.vector('henk') ) 
+# print model.predict_proba( delta.vector('test') ) 
